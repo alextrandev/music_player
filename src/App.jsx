374fa0fe294d417d2d031923f2f3ebec376201bd
Alex Tran <tranduclong092@gmail.com>
2024-08-23 14:18:08 +0300

    Feat: add some music files and import to state component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import { useState } from 'react'
-import './App.css'
-import PlayerControls from './components/PlayerControls'
-import TrackList from './components/TrackList'
+import { useState } from 'react';
+import './App.css';
+import PlayerControls from './components/PlayerControls';
+import TrackList from './components/TrackList';
 import { MusicContext } from './contexts/MusicContext';
+import sinister from './assets/music/sinister.mp3';
+import stomp from './assets/music/stomp.mp3';
+import underwater from './assets/music/underwater.mp3';
 
 function App() {
-  const [state, setState] = useState();
+  const [state, setState] = useState({
+    currentTrackIndex: 0,
+    isPlaying: false,
+    audioPlayer: new Audio(),
+    tracks: [
+      {
+        name: 'Sinister Music ...',
+        file: sinister
+      },
+      {
+        name: 'Stomp Music ...',
+        file: stomp
+      },
+      {
+        name: 'Sinister Music ...',
+        file: underwater
+      }
+    ],
+  });
 
   return (
     <MusicContext.Provider value={[state, setState]}>
