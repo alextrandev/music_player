1dc44976f3615bfe900e709522aaeadb7868f1b5
Alex Tran <tranduclong092@gmail.com>
2024-08-23 12:46:56 +0300

    Feat: add 3 play track functions

--- src/hooks/useMusicPlayer.js
+++ src/hooks/useMusicPlayer.js
@@ -4,20 +4,55 @@ import { MusicContext } from "../contexts/MusicContext";
 const useMusicPlayer = () => {
   const [state, setState] = useContext(MusicContext);
 
-  // function 1 toogle play
   const togglePlay = () => {
     setState({...state, isPlaying: !state.isPlaying});
 
     if (state.isPlaying) {
-      state.audioPlaying.pause();
+      state.audioPlayer.pause();
     } else {
-      state.audioPlaying.pause();
+      state.audioPlayer.pause();
     }
   }
 
+  const playTrack = index => {
+    if (index == state.currentTrackIndex) {
+      togglePlay();
+    } else {
+      state.audioPlayer.pause();
+      state.audioPlayer = new Audio(state.tracks[index].file);
+      state.audioPlayer.play();
+      setState({ ...state, currentTrackIndex: index, isPlaying: true})
+    }
+  }
 
+  const playNextTrack = () => {
+    let newIndex;
+    state.currentTrackIndex === state.tracks.length - 1 ? (newIndex = 0) : (newIndex = state.currentTrackIndex + 1);
+    playTrack(newIndex); 
+  }
 
-  return {};
+  const playPreviousTrack = () => {
+    let newIndex;
+    state.currentTrackIndex === state.tracks.length - 1 ? (state.tracks.length - 1) : (newIndex = state.currentTrackIndex - 1);
+    playTrack(newIndex); 
+  }
+
+  const playRandomTrack = () => {
+    let randomIndex = Math.random() * tracks.length - 1;
+    playTrack(randomIndex);
+  }
+
+  return {
+    togglePlay,
+    playTrack,
+    playNextTrack,
+    playPreviousTrack,
+    playRandomTrack,
+    currentTrackIndex: state.currentTrackIndex,
+    currentTrackName: state.currentTrackIndex !== null && state.tracks[state.currentTrackIndex].name,
+    trackList: state.tracks,
+    isPlaying: state.isPlaying,
+  };
 };
 
 export { useMusicPlayer };
